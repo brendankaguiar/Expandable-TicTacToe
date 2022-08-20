603c5b898e02291c1209df44ae7d56611f5a383a
Brendan Aguiar <50648429+brendankaguiar@users.noreply.github.com>
2022-08-19 17:17:44 -0700

    Update Minimax.h

--- Minimax.h
+++ Minimax.h
@@ -1,24 +1,19 @@
 #ifndef Minimax_H
 #define Minimax_H
-#include "Board.hpp"
+
 class Minimax
 {
 public:
 	Minimax();
-	void to_move(int state[][7]);
-	bool is_terminal(int state[][7]);
-	int utility(int state[][7]);
-	void actions(int a[], int state[][7]);
-	void result(int state[][7], int a);
-	void max_value(int state[][7], int move[]);
-	void min_value(int state[][7], int move[]);
-	bool check_for_winner(const int state[][7], const int utility) const;
-	bool check_vertical(const int state[][7], const int utility) const;
-	bool check_diagN(const int state[][7], const int utility) const;
-	bool check_diagP(const int state[][7], const int utility) const;
-	bool check_for_tie(const int state[][7]) const;
+	void to_move(int state[][3]);
+	bool is_terminal(int state[][3]);
+	int utility(int state[][3]);
+	void actions(int a[][2], int state[][3]);
+	void result(int state[][3], int a[]);
+	void max_value(int state[][3], int move[]);
+	void min_value(int state[][3], int move[]);
 private:
-	int transition[6][7];
+	int transition[3][3];
 	int player;
 };
 #endif
